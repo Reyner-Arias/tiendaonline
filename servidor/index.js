830ff848c6c26e607cdc2b497a24623fa4fadf50
Jose Arce <josearce@estudiantec.cr>
2021-10-14 11:36:12 -0600

    OrientDB connected

---
+++ servidor/index.js
@@ -0,0 +1,138 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser')
+
+const OrientDBClient = require("orientjs").OrientDBClient;
+let sessionOrient;
+
+OrientDBClient.connect({
+    host: "localhost",
+    port: 2424
+}).then(client => {
+    client.session({ name: "TiendaOnline", username: "root", password: "prueba1" })
+        .then(session => {
+            sessionOrient = session;
+        });
+}).then(()=> {
+
+});
+
+app.get('/api/getAllProducts', (req, res) => {
+    sessionOrient.query("SELECT * FROM PRODUCTS")
+        .all()
+        .then((results)=> {
+            res.send(results);
+        });
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+
+
+app.post('/api/newUser/:first_name/:last_name/:email/:password', function(req, res) {
+    const first_name = req.params.first_name;
+    const last_name = req.params.last_name;
+    const email = req.params.email;
+    const password = req.params.password;
+
+    console.log(first_name, last_name, email, password)
+    sessionOrient.insert().into("USERS")
+        .set({
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            password: password
+        })
+        .one()
+        .then((attractions) => {
+            console.log(attractions);
+            res.send(attractions);
+        });
+});
+
+app.post('/api/newShoppingCart/:email', function(req, res) {
+    const email = req.params.email;
+
+    sessionOrient.insert().into("SHOPPING_CAR")
+        .set({
+            id: email,
+        })
+        .one()
+        .then((attractions) => {
+            console.log(attractions);
+            res.send(attractions);
+        });
+});
+
+app.post('/api/newRelation/:riduser/:ridcart', function(req, res) {
+    const riduser = req.params.riduser;
+    const ridcart = req.params.ridcart;
+
+    console.log(riduser, ridcart)
+
+    sessionOrient.query("create edge BELONGS FROM :ridcart TO :riduser", {params: { ridcart: ridcart, riduser:riduser}})
+        .then((attractions) => {
+            console.log(attractions);
+        });
+});
+
+
+app.get('/api/user/login/:email/:password', (req,res) => {
+    const email = req.params.email;
+    const password = req.params.password;
+    sessionOrient.query("SELECT FROM USERS where email = :email and password = :password", {params: { email: email, password:password}})
+        .on("data", data => {
+            console.log(data);
+            res.send(data);
+        })
+        .on('error',(err)=> {
+            console.log(err);
+        })
+        .on("end", () => {
+            console.log("End of the stream");
+        });
+})
+app.get('/api/getBillProduct/:id', (req,res) => {
+    const id = req.params.id;
+
+    sessionOrient.query("select expand(out) from (traverse in_HAVE from (select * from BILLS where @rid=:id)) where @class='HAVE'", {params: { id:id }})
+        .all()
+        .then((results)=> {
+            res.send(results);
+        });
+})
+
+app.get('/api/getBillShoppingCar/:id', (req,res) => {
+    const id = req.params.id;
+
+    sessionOrient.query("select expand(out) from (traverse in_HAVE from (select * from SHOPPING_CAR where @rid=:id)) where @class='HAVE'", {params: { id:id }})
+        .all()
+        .then((results)=> {
+            res.send(results);
+        });
+})
+
+
+app.post('/api/addToShoppingCart/:product_id/:last_name/:email/:password', function(req, res) {
+    const first_name = req.params.first_name;
+    const last_name = req.params.last_name;
+    const email = req.params.email;
+    const password = req.params.password;
+
+    console.log(first_name, last_name, email, password)
+    sessionOrient.insert().into("USERS")
+        .set({
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            password: password
+        })
+        .one()
+        .then((attractions) => {
+            console.log(attractions);
+            res.send(attractions);
+        });
+});
+
+app.listen(3000, () => console.log("Running on port 3000...."));
